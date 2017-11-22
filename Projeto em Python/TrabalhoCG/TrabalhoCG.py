3a1458557a658efdfd92c2e2296e151707aedbf3
Victor Olimpio <victorwebwork@gmail.com>
2017-11-22 21:34:48 -0200

    Adiciona o trabalho 2

--- Projeto em Python/TrabalhoCG/TrabalhoCG.py
+++ Projeto em Python/TrabalhoCG/TrabalhoCG.py
@@ -4,17 +4,19 @@ from graphics import *
 import numpy
 import time
 
+
 def clear_window(win):
     for item in win.items[:]:
         item.undraw()
     win.update()
 
+
 def mouse_click(window, points, vertices):
     click = window.getMouse().clone()
-    result = Rectangle(Point(-1,-1),Point(0,0))
+    result = Rectangle(Point(-1, -1), Point(0, 0))
     for i in range(len(points)):
-        if((click.getX() >= points[i][0].x) and (points[i][1].x >= click.getX()) and
-           (click.getY() >= points[i][0].y) and (points[i][1].y >= click.getY())):
+        if ((click.getX() >= points[i][0].x) and (points[i][1].x >= click.getX()) and
+                (click.getY() >= points[i][0].y) and (points[i][1].y >= click.getY())):
             pontoAtual = Point(vertices[i][0], vertices[i][1])
             point = Circle(pontoAtual, 6)
             point.setOutline("black")
@@ -42,37 +44,37 @@ def main():
     window.setBackground(color_rgb(255, 255, 255))
 
     solid_vertices = [[100, 200, 10],
-                      [150, 150, 10],
+                      [110, 130, 10],
                       [150, 80, 10],
-                      [220, 80, 10],
+                      [205, 45, 10],
                       [270, 30, 10],
-                      [320, 80, 10],
+                      [335, 45, 10],
                       [390, 80, 10],
-                      [390, 150, 10],
+                      [430, 130, 10],
                       [440, 200, 10],
-                      [390, 250, 10],
+                      [430, 265, 10],
                       [390, 320, 10],
-                      [320, 320, 10],
+                      [335, 355, 10],
                       [270, 370, 10],
-                      [220, 320, 10],
+                      [205, 355, 10],
                       [150, 320, 10],
-                      [150, 250, 10],
+                      [110, 265, 10],
                       [100, 200, 50],
-                      [150, 150, 50],
+                      [110, 130, 50],
                       [150, 80, 50],
-                      [220, 80, 50],
+                      [205, 45, 50],
                       [270, 30, 50],
-                      [320, 80, 50],
+                      [335, 45, 50],
                       [390, 80, 50],
-                      [390, 150, 50],
+                      [430, 130, 50],
                       [440, 200, 50],
-                      [390, 250, 50],
+                      [430, 265, 50],
                       [390, 320, 50],
-                      [320, 320, 50],
+                      [335, 355, 50],
                       [270, 370, 50],
-                      [220, 320, 50],
+                      [205, 355, 50],
                       [150, 320, 50],
-                      [150, 250, 50]]
+                      [110, 265, 50]]
 
     star = [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [8, 9], [9, 10], [10, 11],
             [11, 12], [12, 13], [13, 14], [14, 15], [15, 0], [0, 16], [1, 17], [2, 18], [3, 19],
@@ -81,28 +83,28 @@ def main():
             [22, 23], [23, 24], [24, 25], [25, 26], [26, 27], [27, 28], [28, 29], [29, 30], [30, 31], [31, 16]]
 
     faces = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-              [0, 16, 17, 1],
-              [1, 17, 18, 2],
-              [2, 18, 19, 3],
-              [3, 19, 20, 4],
-              [4, 20, 21, 5],
-              [5, 21, 22, 6],
-              [6, 22, 23, 7],
-              [7, 23, 24, 8],
-              [8, 24, 25, 9],
-              [9, 25, 26, 10],
-              [10, 26, 27, 11],
-              [11, 27, 28, 12],
-              [12, 28, 29, 13],
-              [13, 29, 30, 14],
-              [14, 30, 31, 15],
-              [15, 31, 16, 0],
-              [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]]
-
-
+             [0, 16, 17, 1],
+             [1, 17, 18, 2],
+             [2, 18, 19, 3],
+             [3, 19, 20, 4],
+             [4, 20, 21, 5],
+             [5, 21, 22, 6],
+             [6, 22, 23, 7],
+             [7, 23, 24, 8],
+             [8, 24, 25, 9],
+             [9, 25, 26, 10],
+             [10, 26, 27, 11],
+             [11, 27, 28, 12],
+             [12, 28, 29, 13],
+             [13, 29, 30, 14],
+             [14, 30, 31, 15],
+             [15, 31, 16, 0],
+             [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]]
 
     active = True
     while active:
+        colors = [50, 100, 20]
+
         solid = Solid()
         solid.add_vertices(numpy.array(solid_vertices))
         solid.add_edges(star)
@@ -142,6 +144,7 @@ def main():
             sub_title.draw(window)
             angle = input("Insira o valor de um angulo")
             solid.rot_x(angle)
+
         if anwser == 2:
             clear_window(window)
             subtitle_message = "Rotação no eixo Y"
@@ -149,6 +152,7 @@ def main():
             sub_title.draw(window)
             angle = input("Insira o valor de um angulo")
             solid.rot_y(angle)
+
         if anwser == 3:
             clear_window(window)
             subtitle_message = "Rotação no eixo Z"
@@ -156,18 +160,43 @@ def main():
             sub_title.draw(window)
             angle = input("Insira o valor de um angulo")
             solid.rot_z(angle)
+
         if anwser == 4:
             clear_window(window)
             subtitle_message = "Projeção Isométrica com angulo em x 35.26 e em y 45"
             sub_title = Text(Point(683, 30), subtitle_message)
             sub_title.draw(window)
             solid.isometric_projection(35.26, 45)
+
+            option = input("Escolha uma das opções abaixo, ou digite N para sair:\n"
+                           "1- Para exibir em RGB:\n"
+                           "2- Para exibir em CMY:\n"
+                           "3- Para exibir em HSV:\n")
+
+            if option == 1:
+                solid.paint(faces, window, colors, 1)
+            if option == 2:
+                solid.paint(faces, window, colors, 2)
+            if option == 3:
+                solid.paint(faces, window, colors, 3)
         if anwser == 5:
             clear_window(window)
             subtitle_message = "Projeção Obliqua Cabinet"
             sub_title = Text(Point(683, 30), subtitle_message)
             sub_title.draw(window)
             solid.oblique(0.5, 13 * 5)
+
+            option = input("Escolha uma das opções abaixo, ou digite N para sair:\n"
+                           "1- Para exibir em RGB:\n"
+                           "2- Para exibir em CMY:\n"
+                           "3- Para exibir em HSV:\n")
+            if option == 1:
+                solid.paint(faces, window, colors, 1)
+            if option == 2:
+                solid.paint(faces, window, colors, 2)
+            if option == 3:
+                solid.paint(faces, window, colors, 3)
+
         if anwser == 6:
             clear_window(window)
             subtitle_message = "Projeção em Perspectiva com 1 ponto de fuga"
@@ -175,23 +204,33 @@ def main():
             sub_title.draw(window)
             solid.perspective(100 + 10 * 13)
 
+            option = input("Escolha uma das opções abaixo, ou digite N para sair:\n"
+                           "1- Para exibir em RGB:\n"
+                           "2- Para exibir em CMY:\n"
+                           "3- Para exibir em HSV:\n")
+            if option == 1:
+                solid.paint(faces, window, colors, 1)
+            if option == 2:
+                solid.paint(faces, window, colors, 2)
+            if option == 3:
+                solid.paint(faces, window, colors, 3)
+
         title = Text(Point(683, 10), message)
         title.draw(window)
 
-
-
-
         points = solid.paint_points(window)
 
-        solid.draw(window, 0.05)
-        solid.paint(faces, window)
+        #solid.draw(window, 0.05)
+
+
+
         title.undraw()
         sub_title.undraw()
 
         stil = True
-        while(stil):
+        while (stil):
             subtitle_message = "Clique em um dos quadrados na esquerda da tela para destacar \n " \
-                      "o respectivo ponto no sólido, ou clique em qualquer lugar da tela para prosseguir."
+                               "o respectivo ponto no sólido, ou clique em qualquer lugar da tela para prosseguir."
             sub_title = Text(Point(683, 30), subtitle_message)
             sub_title.draw(window)
             update(60)
@@ -207,4 +246,6 @@ def main():
 
     window.getMouse()
     window.close()
+
+
 main()
--- Projeto em Python/TrabalhoCG/solid.py
+++ Projeto em Python/TrabalhoCG/solid.py
@@ -1,8 +1,15 @@
 import numpy
+import math
+from numpy.distutils.system_info import numarray_info
+
 from graphics import *
 
 class Solid:
+
     def __init__(self):
+        self.p = [765, 301, -1000]
+        self.rgb_scale = 255
+        self.cmyk_scale = 100
         self.vertices = numpy.zeros((0, 4))
         self.edges = []
 
@@ -160,10 +167,10 @@ class Solid:
             end_circle = Circle(end_point, 3)
             line = Line(start_point, end_point)
 
-            start_circle.setFill("yellow")
-            end_circle.setFill("red")
+            #start_circle.setFill("yellow")
+            #end_circle.setFill("red")
 
-            start_circle.draw(window)
+            #start_circle.draw(window)
             time.sleep(delay)
             update(60)
 
@@ -171,41 +178,275 @@ class Solid:
             time.sleep(delay)
             update(60)
 
-            end_circle.draw(window)
+            #end_circle.draw(window)
             time.sleep(delay)
             update(60)
 
-            end_circle.setFill("yellow")
+            #end_circle.setFill("yellow")
             time.sleep(delay)
             update(60)
 
-    def paint(self, faces, window):
-        colors = ["skyblue", "violet", "green", "purple", "pink", "blue", "brown", "indigo", "grey", "orange"]
+    def normal(self, vertice1, vertice2, vertice3):
+        v1 = numpy.subtract(vertice2, vertice1)
+        v2 = numpy.subtract(vertice3, vertice1)
+        normal = numpy.cross(v1[:3], v2[:3])
+        return normal
+
+    def normals(self):
+        normals = []
+        normals.append(self.normal(self.vertices[0], self.vertices[1], self.vertices[2]))
+        normals.append(self.normal(self.vertices[0], self.vertices[16], self.vertices[17]))
+        normals.append(self.normal(self.vertices[1], self.vertices[17], self.vertices[18]))
+        normals.append(self.normal(self.vertices[2], self.vertices[18], self.vertices[19]))
+        normals.append(self.normal(self.vertices[3], self.vertices[19], self.vertices[20]))
+        normals.append(self.normal(self.vertices[4], self.vertices[20], self.vertices[21]))
+        normals.append(self.normal(self.vertices[5], self.vertices[21], self.vertices[22]))
+        normals.append(self.normal(self.vertices[6], self.vertices[22], self.vertices[23]))
+        normals.append(self.normal(self.vertices[7], self.vertices[23], self.vertices[24]))
+        normals.append(self.normal(self.vertices[8], self.vertices[24], self.vertices[25]))
+        normals.append(self.normal(self.vertices[9], self.vertices[25], self.vertices[26]))
+        normals.append(self.normal(self.vertices[10], self.vertices[26], self.vertices[27]))
+        normals.append(self.normal(self.vertices[11], self.vertices[27], self.vertices[28]))
+        normals.append(self.normal(self.vertices[12], self.vertices[28], self.vertices[29]))
+        normals.append(self.normal(self.vertices[13], self.vertices[29], self.vertices[30]))
+        normals.append(self.normal(self.vertices[14], self.vertices[30], self.vertices[31]))
+        normals.append(self.normal(self.vertices[15], self.vertices[31], self.vertices[16]))
+        normals.append(self.normal(self.vertices[22], self.vertices[21], self.vertices[20]))
+
+        return normals
+
+    def visibles(self):
+        visibles = []
+        normals = self.normals()
+        visibles.append(self.back_face_culling(normals[0], self.vertices[0]))
+        visibles.append(self.back_face_culling(normals[1], self.vertices[0]))
+        visibles.append(self.back_face_culling(normals[2], self.vertices[1]))
+        visibles.append(self.back_face_culling(normals[3], self.vertices[2]))
+        visibles.append(self.back_face_culling(normals[4], self.vertices[3]))
+        visibles.append(self.back_face_culling(normals[5], self.vertices[4]))
+        visibles.append(self.back_face_culling(normals[6], self.vertices[5]))
+        visibles.append(self.back_face_culling(normals[7], self.vertices[6]))
+        visibles.append(self.back_face_culling(normals[8], self.vertices[7]))
+        visibles.append(self.back_face_culling(normals[9], self.vertices[8]))
+        visibles.append(self.back_face_culling(normals[10], self.vertices[9]))
+        visibles.append(self.back_face_culling(normals[11], self.vertices[10]))
+        visibles.append(self.back_face_culling(normals[12], self.vertices[11]))
+        visibles.append(self.back_face_culling(normals[13], self.vertices[12]))
+        visibles.append(self.back_face_culling(normals[14], self.vertices[13]))
+        visibles.append(self.back_face_culling(normals[15], self.vertices[14]))
+        visibles.append(self.back_face_culling(normals[16], self.vertices[15]))
+        visibles.append(self.back_face_culling(normals[17], self.vertices[18]))
+
+        return visibles
+
+
+    def back_face_culling(self, normal, vertice):
+        if numpy.inner(numpy.subtract(vertice[:3], self.p), normal) > 0:
+            return True
+        return False
+
+    def unit_vector(self, vector):
+        return vector / numpy.linalg.norm(vector)
+
+    def angle(self, v1, v2):
+        v1_u = self.unit_vector(v1)
+        v2_u = self.unit_vector(v2)
+        return numpy.arccos(numpy.clip(numpy.dot(v1_u, v2_u), -1.0, 1.0))
+
+    def vector_light(self, normal):
+        return numpy.subtract(self.p, normal)
+
+    def lights(self):
+        lights = []
+        for normal in self.normals():
+            lights.append(self.vector_light(normal))
+        return lights
+
+
+    def rgb_to_cmyk(self, r, g, b):
+        result = []
+        if (r == 0) and (g == 0) and (b == 0):
+            # black
+            c = 0
+            m = 0
+            y = 0
+            k = self.cmyk_scale
+            result.append(c)
+            result.append(m)
+            result.append(y)
+            result.append(k)
+            return result
+
+        c = 1 - r / float(self.rgb_scale)
+        m = 1 - g / float(self.rgb_scale)
+        y = 1 - b / float(self.rgb_scale)
+
+        min_cmy = min(c, m, y)
+        c = abs((c - min_cmy))
+        m = abs((m - min_cmy))
+        y = abs((y - min_cmy))
+        k = min_cmy
+
+        c = c * self.cmyk_scale
+        m = m * self.cmyk_scale
+        y = y * self.cmyk_scale
+        k = k * self.cmyk_scale
+
+        result.append(c)
+        result.append(m)
+        result.append(y)
+        result.append(k)
+
+        return result
+
+    def cmyk_to_rgb(self, c, m, y, k):
+        result = []
+
+        r = abs(self.rgb_scale * (1.0 - c / float(self.cmyk_scale)) * (1.0 - k / float(self.cmyk_scale)))
+        g = abs(self.rgb_scale * (1.0 - m / float(self.cmyk_scale)) * (1.0 - k / float(self.cmyk_scale)))
+        b = abs(self.rgb_scale * (1.0 - y / float(self.cmyk_scale)) * (1.0 - k / float(self.cmyk_scale)))
+
+        result.append(r)
+        result.append(g)
+        result.append(b)
+        return result
+
+    def rgb_to_hsv(self, r, g, b):
+
+        mx = max(r, g, b)
+        mn = min(r, g, b)
+
+        delta = mx - mn
+        if mx == mn:
+            h = 0
+        elif mx == r:
+            h = (60 * (abs(g - b) / delta) + 360)
+        elif mx == g:
+            h = (60 * (abs(b - r) / delta) + 120)
+        elif mx == b:
+            h = (60 * (abs(r - g) / delta) + 240)
+        if mx == 0:
+            s = 0
+        else:
+            s = delta / mx
+        v = mx
+
+        result = []
+        result.append(h)
+        result.append(s)
+        result.append(v)
+        return result
+
+    def hsv_to_rgb(self, h, s, v):
+        h = float(h)
+        s = float(s)
+        v = float(v)
+
+        h60 = h / 60.0
+        h60f = math.floor(h60)
+        hi = int(h60f) % 6
+
+        f = h60 - h60f
+        p = v * (1 - s)
+        q = v * (1 - f * s)
+        t = v * (1 - (1 - f) * s)
+        r, g, b = 0, 0, 0
+
+        if hi == 0:
+            r, g, b = v, t, p
+        elif hi == 1:
+            r, g, b = q, v, p
+        elif hi == 2:
+            r, g, b = p, v, t
+        elif hi == 3:
+            r, g, b = p, q, v
+        elif hi == 4:
+            r, g, b = t, p, v
+        elif hi == 5:
+            r, g, b = v, p, q
+        r, g, b = int(r * 255), int(g * 255), int(b * 255)
+
+        result = []
+        result.append(r)
+        result.append(g)
+        result.append(b)
+        return result
+
+    def paint(self, faces, window, colors, type):
+        #colors = ["skyblue", "violet", "green", "purple", "pink", "blue", "brown", "indigo", "grey", "orange"]
+        coef = 0.7
         j = 0
+        l = 0
+        visibles = self.visibles()
+        normals = self.normals()
+        lights = self.lights()
         for face in faces:
             points = []
-            for i in range(len(face)):
-                point = Point(self.vertices[face[i]][0], self.vertices[face[i]][1])
-                points.append(point)
 
-            f = Polygon(points)
-            f.setFill(colors[j])
-            f.setOutline("black")
-            f.draw(window)
-            time.sleep(0.2)
+            if visibles[l]:
+                for i in range(len(face)):
+                    point = Point(self.vertices[face[i]][0], self.vertices[face[i]][1])
+                    points.append(point)
 
-            update(60)
+                f = Polygon(points)
+                v1 = numpy.array(normals[l])
+                v2 = numpy.array(lights[l])
 
-            if (j > 8):
-                j = 0
-            j += 1
+                cos = self.angle(v1, v2)
+
+                if type == 1:
+                    r = abs(coef * colors[0] * cos)
+                    g = abs(coef * colors[1] * cos)
+                    b = abs(coef * colors[2] * cos)
+
+                if type == 2:
+
+                    converted = self.rgb_to_cmyk(colors[0], colors[1], colors[2])
+
+                    r = abs(coef * converted[0] * cos)
+                    g = abs(coef * converted[1] * cos)
+                    b = abs(coef * converted[2] * cos)
+                    k = abs(coef * converted[3] * cos)
+
+                    converted = self.cmyk_to_rgb(r, g, b, k)
+
+                    r = converted[0]
+                    g = converted[1]
+                    b = converted[2]
+
+                if type == 3:
+                    converted = self.rgb_to_hsv(colors[0], colors[1], colors[2])
+
+                    r = abs(coef * converted[0] * cos)
+                    g = abs(coef * converted[1] * cos)
+                    b = abs(coef * converted[2] * cos)
+
+                    converted = self.hsv_to_rgb(r, g, b)
+
+                    r = converted[0]
+                    g = converted[1]
+                    b = converted[2]
+
+                f.setFill(color_rgb(r, g, b))
+                f.setOutline(color_rgb(r, g, b))
+                f.draw(window)
+                time.sleep(0.2)
+
+                update(60)
+
+                if (j > 8):
+                    j = 0
+                j += 1
+
+            l += 1
 
     def paint_points(self, window):
         i = 15
         j = 0
         squares = []
         for vertice in self.vertices:
-            s = ', '.join(str(e) for e in (vertice[0], vertice[1], vertice[2]))
+            string_number = str(j)
+            s = string_number + ': ' + ', '.join(str(e) for e in (vertice[0], vertice[1], vertice[2]))
 
             squares.append([" "] * 2)
 
@@ -226,3 +467,5 @@ class Solid:
             j+= 1
 
         return squares
+
+
